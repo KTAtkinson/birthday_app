b036ddabc3dcbd41c03ca590ad441b0ecbca35a6
OpenShift guest <kaitlin@atkinson.mn>
2014-01-01 15:22:25 -0500

    Add pylint args so it doesn't find E1101.

--- birthday_app/__init__.py
+++ birthday_app/__init__.py
@@ -1,5 +1,1 @@
 ""
-def nothing():
-    nothing
-haha
-    
--- birthday_app/hooks/python/lint.py
+++ birthday_app/hooks/python/lint.py
@@ -4,22 +4,32 @@ import subprocess
 
 
 def pylint(file_path):
-	if ("__init__.py" in file_path or 
-	    "__script__=hook" in open(file_path).read()):
-    return [True, None]
+  """ runs pylint on file found at file_path"""
+  if ("__init__.py" in file_path or 
+      "# __script__=hook" in open(file_path).read() and
+      "lint.py" not in file_path):
+    return [None, None]
 	
-	args = ["pylint",
+  args = ["pylint",
 			"""--rcfile=./standard.rc""",
 			"--output-format",
 			"parseable",
 			"--reports",
 			"n",
+			"-d",
+			"E1101",
 			file_path]
-	popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-	stdout, stderr = popen.communicate()
+  popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+  stdout, stderr = popen.communicate()
+  
+  if stderr:
+    return [False, stderr]
 	
-	if stdout:
-		errors = stdout[:-1].replace("{0}:".format(file_path), "line ")
-		return [False, "\t" + "\n\t".join(errors.split("\n")) + "\n"]
+  if stdout:
+    errors = stdout[:-1].replace("{0}:".format(file_path), "line ")
+    return [False, "\t" + "\n\t".join(errors.split("\n")) + "\n"]
 	
-	return [True, None]
+  return [True, None]
+
+if __name__ == '__main__':
+  pass
